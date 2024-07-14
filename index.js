6c5971cfd704b922da5d697c09d1d014293cddd5
Oltiariq3561 <oltiariq356@gmail.com>
2024-07-14 11:32:53 -0700

    salomat

---
+++ index.js
@@ -0,0 +1,84 @@
+// 1. Berilgan sonni ikkiga bo'lib, natijani butun qismiga oshiring va qoldig'ini hisoblang.
+// let a = +prompt('son kiriting');
+// let natija = Math.floor(a / 2);
+// let qoldiq = a % 2;
+// console.log(natija);
+// console.log(qoldiq);
+
+// 2. Berilgan ikkita sonning birinchi son ikkinchi sondan katta bo'lsa, ko'paytmasini, kichik bo'lsa, yig'indisini, teng bo'lsa ayirmasini toping.
+// let a = +prompt('son kiriting');
+// let b = +prompt('son kiriting');
+// if (a > b) {
+//     let kopaytma = a * b;
+//     console.log(kopaytma);
+// }else if (a == b) {
+// let ayirma = a - b;
+// console.log(ayirma);
+// }else {
+//     let yigindi = a + b;
+// console.log(yigindi);
+// }
+
+// 3. Berilgan sonning faktorialini toping.
+// let a = +prompt('son kiritinng');
+// let result = 1;
+// for (let i = 1; i <= a ; i++ ){
+// result = result*i 
+// }
+// console.log(result);
+
+// 4. Berilgan uchta sonning kvadratlari yig'indisini toping.
+// let a = +prompt('son kirit');
+// let b = +prompt('son kirit');
+// let c = +prompt('son kirit');
+// let resa = a * a ;
+// let resb = b * b ;
+// let resc = c * c ;
+// let result = resa + resb + resc;
+// console.log(result);
+
+// 5. Berilgan ikki sonning darajasini hisoblang (a^b).
+// let a = +prompt('son kiriting');
+// let b = +prompt('son kiriting');
+// let result = a**b;
+// console.log(result);
+
+// 6-masala
+// 6. Berilgan sonning kvadrat ildizini hisoblab, natijani eng yaqin butun songa yaxlitlang.
+// let a = +prompt('son kiriting');
+// let ildiz = Math.sqrt(a);
+// let yaxlid = Math.round(ildiz);
+// console.log(ildiz);
+// console.log(yaxlid);
+
+// 7. Berilgan ikki sonni ketma-ket 5 marta ko'paytiring va natijalarni alohida chop eting.
+// let a = +prompt('son kiriting');
+// let b = +prompt('son kiriting');
+// res = 1;
+// result = 1;
+// for(let i = 1; i <= 5 ; i++){
+// res = res * a;
+// }
+// console.log(res);
+// for(let n = 1; n <= 5 ; n++){
+// result = result * b;
+// }
+// console.log(result);
+
+// 8. Berilgan sonni 10 ga ko'paytirib, natijadan 7 ni ajratib oling.
+// let a = +prompt('son kiriting');
+// let result = a * 10 - 7;
+// console.log(result);
+
+// 9. Berilgan sonning uchinchi darajali ildizini toping.
+// let a = +prompt('son kirit');
+// let result = a*a*a;
+// console.log(result);
+
+// 10. Berilgan uchta sonning o'rtacha geometrik qiymatini hisoblang (kublarining o'rtacha ildizi).
+// let a = +prompt('son kiritin');
+// let b = +prompt('son kiritin');
+// let c = +prompt('son kiritin');
+// let res = Math.pow(a*b*c,1/3);
+// let butun = Math.round(res)
+// console.log(butun);
